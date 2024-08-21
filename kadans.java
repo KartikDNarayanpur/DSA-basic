e4d4dbca93d5eb2d9f4d24045c270bc99cfe0c20
KartikDNarayanpur <152896474+KartikDNarayanpur@users.noreply.github.com>
2024-08-22 00:01:52 +0530

    Update kadans.java

--- kadans.java
+++ kadans.java
@@ -1,3 +1,53 @@
-public class kadans{
+import java.util.*;
+public class practice {
+    public static boolean  check_positive_ele(int num[]) {
+        
+        for (int ele : num) {
+            if (ele > 0) {
+                return true;
+            }
+        }
+        return false;
+    }
     
+
+
+    public static void kadans(int num[]){
+        int ms= Integer.MIN_VALUE;
+        int cs=0;
+        for (int i=0;i<num.length;i++){
+            cs = cs+num[i];
+            if (cs<0){
+                cs=0;
+            }
+            ms = Math.max(cs,ms);
+        }
+    System.out.println("max sum is "+ms);
+        
+    }
+
+    public static void main(String[] args) {
+        int num[]={ -2,3,-4,-1,-2,1,5,-3};
+       // kadans(num);
+        if(check_positive_ele(num)==false ){
+            int ms=0;
+            for (int i=0;i<num.length;i++){
+           
+               ms= ms + num[i];
+                
+
+        }
+        System.out.println("max sum is "+ms);
+    }
+        else {
+          
+                
+                kadans(num);
+
+            
+        }
+    
+
+    }
 }
+
